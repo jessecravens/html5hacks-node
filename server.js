51e591a34db231bfefb50e2691df6f1d05d99e25
Jesse Cravens <jesse.cravens@gmail.com>
2012-01-19 14:15:41 -0600

    added new library dependencies

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var sys = require('sys'),
 
-var app = express.createServer(express.logger());
+
+// 3rd Party Libraries
+express = require('express'),
+io = require('socket.io'),
+sty = require('sty/lib/sty'),
+haml = require('haml/lib/haml'),
+jade = require('jade/lib/jade'),
+express = require('express/lib/express'),
+jsdom = require("jsdom/lib/jsdom"),
+mongoose = require('mongoose'),
+
+app = express.createServer(express.logger());
 
 	app.get('/', function(request, response) {
-  		response.send('html5hacks-setup');
+  		response.send('html5hacks-node');
 	});
 
 	var port = process.env.PORT || 3333;
