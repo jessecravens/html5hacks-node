23f2a791114633da1678b692c27f068f2622dad2
Jesse Cravens <jesse.cravens@gmail.com>
2012-02-05 13:26:04 -0600

    added Jade module, app now loading jquery and yui

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
-var sys = require('sys'),
 
-
-// 3rd Party Libraries
-express = require('express'),
-io = require('socket.io'),
-sty = require('sty/lib/sty'),
-haml = require('haml/lib/haml'),
-jade = require('jade/lib/jade'),
-express = require('express/lib/express'),
-jsdom = require("jsdom/lib/jsdom"),
-mongoose = require('mongoose'),
-
-app = express.createServer(express.logger());
-
-	app.get('/', function(request, response) {
-  		response.send('html5hacks-node');
-	});
-
-	var port = process.env.PORT || 3333;
+function start() {	
+	
+	var sys = require('sys'),
+	// 3rd Party Libraries
+	express = require('express')
+	
+	// Express app
+	app = express.createServer(express.logger());
+	
+	var port = process.env.PORT || 3232;
 		app.listen(port, function() {
   		console.log("Listening on " + port);
 	});
+	
+	app.configure(function(){
+		app.set('view engine', 'jade');
+		app.set('views', __dirname + '/views');
+		app.set('view options', {
+		  layout: false
+		});
+ 	});
+	
+	// app.get('/', function(request, response) {
+	// 	response.send('html5hacks-node');
+	// });
+		
+	app.get('/', function(req, res){
+		console.info('hello');
+		res.render('index.jade');
+	  });
+
+
+ 	console.log("Server started.");
+	}; 
+	
+
+exports.start = start;
