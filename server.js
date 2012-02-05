5311d79c30e67598a8b61993fe665be6e67e42b4
Jesse Cravens <jesse.cravens@gmail.com>
2012-02-05 14:25:30 -0600

    Added simple Jade test

--- server.js
+++ server.js
@@ -4,13 +4,30 @@
 		var sys = require('sys'),
 
 		// 3rd Party Libraries
-		express = require('express')
+		express = require('express'),
+		jade = require('jade')
 
-		app = express.createServer(express.logger());
-
-			app.get('/', function(request, response) {
-		  		response.send('html5hacks-node');
+			app = express.createServer(express.logger());
+	
+			app.configure(function(){
+			    app.use(express.methodOverride());
+			    app.use(express.bodyParser());
+			    app.use(app.router);
+				app.set('view engine', 'jade');
+				app.set('views', __dirname + '/views');
+				app.set('view options', {
+				  layout: false
+				});
 			});
+
+			// app.get('/', function(request, response) {
+			// 		  		response.send('html5hacks-node');
+			// });
+			
+			app.get('/', function(req, res){
+				console.info('hello');
+				res.render('index.jade');
+			});		
 
 			var port = process.env.PORT || 3232;
 				app.listen(port, function() {
