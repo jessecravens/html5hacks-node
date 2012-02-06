309ea55433bd132db549507dc326182742c42f16
Jesse Cravens <jesse.cravens@gmail.com>
2012-02-05 22:48:46 -0600

    Added more evolved routing, added partial nav.jade, removed server.js and index.js - now all handled in app.js

--- app.js
+++ app.js
@@ -1,38 +1,57 @@
 
-	// Module Dependencies //////////////////////////////////////////////////////////
-	var sys = require('sys'),
-	    express = require('express'), 
-	    routes = require('./routes')
+// Module Dependencies //////////////////////////////////////////////////////////
+var sys = require('sys'),
+    express = require('express'), 
+    routes = require('./routes')
 
-	var app = module.exports = express.createServer();
-
-	// Configuration ////////////////////////////////////////////////////////////////
-		app.configure(function(){
-		  app.set('views', __dirname + '/views');
-		  app.set('view engine', 'jade');
-		  app.use(express.bodyParser());
-		  app.use(express.methodOverride());
-		  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-		  app.use(app.router);
-		  app.use(express.static(__dirname + '/public'));
-		});
-
-		app.configure('development', function(){
-		  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-		});
-
-		app.configure('production', function(){
-		  app.use(express.errorHandler()); 
-		});
-
-	// Routes ////////////////////////////////////////////////////////////////////////
-		app.get('/', routes.index);
+	// print out require paths
+	for (var key in require.paths) {
+	  if (require.paths.hasOwnProperty(key)) {
+	    console.log(require.paths[key]);
+	  }
+	};	
 		
-	// Listen on port 3232 ///////////////////////////////////////////////////////////
-		var port = process.env.PORT || 3232;
-			app.listen(port, function() {
-		 	console.log("Express server listening on port %d in %s mode", 
-						app.address().port, 
-						app.settings.env
-						);
-		});
+	console.log(routes.index)
+	console.log(routes.chapter1)
+
+var app = module.exports = express.createServer();
+
+// Configuration ////////////////////////////////////////////////////////////////
+	app.configure(function(){
+	  app.set('views', __dirname + '/views');
+	  app.set('view engine', 'jade');
+	  app.use(express.bodyParser());
+	  app.use(express.methodOverride());
+	  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+	  app.use(app.router);
+	  app.use(express.static(__dirname + '/public'));
+	});
+
+	app.configure('development', function(){
+	  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	});
+
+	app.configure('production', function(){
+	  app.use(express.errorHandler()); 
+	});
+
+// Routes ////////////////////////////////////////////////////////////////////////
+	app.get('/', routes.index);
+	app.get('/chapter1', routes.chapter1);
+	app.get('/chapter2', routes.chapter2);
+	app.get('/chapter3', routes.chapter3);
+	app.get('/chapter4', routes.chapter4);
+	app.get('/chapter5', routes.chapter5);
+	app.get('/chapter6', routes.chapter6);
+	app.get('/chapter7', routes.chapter7);
+	app.get('/chapter8', routes.chapter8);
+	app.get('/chapter9', routes.chapter9);
+															
+// Listen on port 3232 ///////////////////////////////////////////////////////////
+var port = process.env.PORT || 3232;
+	app.listen(port, function() {
+	 console.log("Express server listening on port %d in %s mode", 
+				app.address().port, 
+				app.settings.env
+				);
+	});
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,44 @@
 
-/*
- * GET home page.
- */
+// GET home page
+  
+  exports.index = function(req, res){
+    res.render('index', { title: 'html5hacks-node' })
+  };
 
-exports.index = function(req, res){
-  res.render('index', { title: 'html5hacks-node' })
-};
+// GET chapter 1 page
+
+  exports.chapter1 = function(req, res){
+    res.render('chapter1', { title: 'Chapter 1 Hacking the Semantical Way' })
+  };
+
+  exports.chapter2 = function(req, res){
+    res.render('chapter2', { title: 'Chapter 2' })
+  };
+
+  exports.chapter3 = function(req, res){
+    res.render('chapter3', { title: 'Chapter 3' })
+  };
+
+  exports.chapter4 = function(req, res){
+    res.render('chapter4', { title: 'Chapter 4' })
+  };
+
+  exports.chapter5 = function(req, res){
+    res.render('chapter5', { title: 'Chapter 5' })
+  };
+
+  exports.chapter6 = function(req, res){
+    res.render('chapter6', { title: 'Chapter 6' })
+  };
+
+  exports.chapter7 = function(req, res){
+    res.render('chapter7', { title: 'Chapter 7' })
+  };
+
+  exports.chapter8 = function(req, res){
+    res.render('chapter8', { title: 'Chapter 8' })
+  };
+
+  exports.chapter9 = function(req, res){
+    res.render('chapter9', { title: 'Chapter 9' })
+  };
